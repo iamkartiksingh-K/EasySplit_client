b4093a50f519095f15c8c1ebed9613bca1d32ff0
iamkartiksingh-k <kartik01022001@gmail.com>
2024-04-04 22:19:34 +0530

    made homepage responsive

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,5 @@
 import { Link, Navigate } from "react-router-dom";
-import {
-	Button,
-	Group,
-	Container,
-	Title,
-	Space,
-	Text,
-	Box,
-	Image,
-	Paper,
-} from "@mantine/core";
+import { Button, Group, Container, Box, Image } from "@mantine/core";
 import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
 import { Logo } from "../assets/CustomIcons";
@@ -22,33 +12,35 @@ function Homepage() {
 			<Container size='xl' className='h-full'>
 				<Group justify='space-between' className='px-5 py-3 w-full '>
 					<Group>
-						<Logo />
-						<p className='text-3xl font-bold'>EasySplit</p>
+						<Box className=''>
+							<Logo />
+						</Box>
+						<p className='md:text-3xl font-bold'>EasySplit</p>
 					</Group>
-					<Group>
-						<Link to={"/register"}>
-							<Button size='lg' variant='subtle' color='myColor'>
-								Register
-							</Button>
+					<div className='flex gap-3 md:gap-10'>
+						<Link
+							to={"/register"}
+							className='text-[#3DCE6D] font-semibold text-sm md:font-bold md:text-lg'>
+							Register
 						</Link>
-						<Link to={"/login"}>
-							<Button size='lg' variant='subtle' color='myColor'>
-								Login
-							</Button>
+						<Link
+							to={"/login"}
+							className='text-[#3DCE6D] font-semibold text-sm md:font-bold md:text-lg'>
+							Login
 						</Link>
-					</Group>
+					</div>
 				</Group>
-				<div className=' flex justify-center items-center pt-32'>
-					<Box className='h-1/2'>
+				<div className='flex flex-col justify-center pt-5 items-center lg:pt-32 lg:flex-row'>
+					<Box className='h-1/2 mb-8 md:mb-0'>
 						<p
-							className='text-7xl font-extrabold text-neutral-950 mb-7 '
+							className='text-3xl md:text-7xl font-extrabold text-neutral-950 mb-7 '
 							style={{
 								fontFamily:
 									" Greycliff CF var(--mantine-font-family)",
 							}}>
 							Split Expenses in Seconds
 						</p>
-						<p className='mb-8 text-2xl text-slate-600'>
+						<p className='mb-8 md:text-2xl text-slate-600'>
 							Forget complicated spreadsheets or awkward "who owes
 							what" conversations. EasySplit's intuitive interface
 							handles all the calculations, ensuring accurate and
